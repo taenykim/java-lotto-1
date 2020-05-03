779385764e869de0272c32a0abb7d38cef840f44
taeeun kim <kimtaeeeny@gmail.com>
2020-05-04 05:21:34 +0900

    test : WinningLotto객체에 Lotto객체가 잘 들어가는지 확인

--- src/modules/WinningLotto.test.ts
+++ src/modules/WinningLotto.test.ts
@@ -1,1 +1,12 @@
 import { WinningLotto } from './WinningLotto'
+import { Lotto } from './Lotto'
+
+describe('WinningLotto객체', () => {
+  it('WinningLotto객체에 Lotto객체가 잘 들어가는지 확인', () => {
+    // given, when
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6])
+    const winningLotto = new WinningLotto(lotto, 3)
+    // then
+    expect(winningLotto.lotto).toStrictEqual(new Lotto([1, 2, 3, 4, 5, 6]))
+  })
+})
