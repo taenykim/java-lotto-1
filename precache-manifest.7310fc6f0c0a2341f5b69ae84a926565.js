166ad44f3632e4e436540d2bb922d5e03cf18842
taeeun kim <kimtaeeeny@gmail.com>
2020-05-04 12:27:18 +0900

    Updates

---
+++ precache-manifest.7310fc6f0c0a2341f5b69ae84a926565.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7bebee37b663076c76b949c8da841d7",
+    "url": "/java-lotto-1/index.html"
+  },
+  {
+    "revision": "658db314018fc6051525",
+    "url": "/java-lotto-1/static/js/2.4e30827e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/java-lotto-1/static/js/2.4e30827e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18b6402cedcdf9accdbd",
+    "url": "/java-lotto-1/static/js/main.79055ea0.chunk.js"
+  },
+  {
+    "revision": "4a69b95d75f5b0125d8a",
+    "url": "/java-lotto-1/static/js/runtime-main.17cfd98a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/java-lotto-1/precache-manifest.7310fc6f0c0a2341f5b69ae84a926565.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/java-lotto-1/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
